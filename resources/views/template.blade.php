7eb766c64e5cfb9847a335b83cdae2c0dc52cd49
marieloujo <detchenoujoan@gmail.com>
2021-04-07 18:00:13 +0100

    upadate

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -73,31 +73,8 @@
             <footer id="main-footer">
                 <div id="footer-bottom">
                     <div class="container clearfix">
-                        <ul class="et-social-icons">
-                            <li class="et-social-icon et-social-facebook">
-                                <a href="#" class="icon">
-                                <span>Facebook</span>
-                                </a>
-                            </li>
-                            <li class="et-social-icon et-social-twitter">
-                                <a href="#" class="icon">
-                                <span>Twitter</span>
-                                </a>
-                            </li>
-                            <li class="et-social-icon et-social-google-plus">
-                                <a href="#" class="icon">
-                                <span>Google</span>
-                                </a>
-                            </li>
-                            <li class="et-social-icon et-social-rss">
-                                <a href="https://www.elegantthemes.com/layouts/feed" class="icon">
-                                <span>RSS</span>
-                            </a>
-                            </li>
-                        </ul>
                         <p id="footer-info">
-                            Designed by <a href="https://www.elegantthemes.com" title="Premium WordPress Themes">
-                            Elegant Themes</a> | Powered by <a href="https://www.wordpress.org">WordPress</a>
+                            Designed by <a href="https://www.marielladetchenou.com" title="">mariella</a></a>
                         </p> 
                     </div> 
                 </div>
